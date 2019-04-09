775f2ddf6591ecaabe9f63460a2ead9d71a154d5
Prasanna <prasanna.dubey1997@gmail.com>
2019-04-09 22:23:21 +0530

    Intial Commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import Burger from './../../components/Burger/Burger'
+import BuildControls from './../../components/Burger/BuildControls/BuildControls';
+
+class BurgerBuilder extends Component {
+
+  state =  {
+      ingredients : {
+        salad : 1,
+        bacon : 1,
+        cheese : 2,
+        meat : 2
+      }
+  }
+
+  render(){
+    return (
+      <>
+        <Burger ingredients={this.state.ingredients}/>
+        <BuildControls />
+      </>
+    );
+  }
+}
+
+export default BurgerBuilder;
